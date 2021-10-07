5d70d9cc46dec8b1bb3964094252643fa494e45a
briandidwhat <91812233+briandidwhat@users.noreply.github.com>
2021-10-07 12:47:46 -0400

    Facebook Pixel Update (V6 - FINALIZING)

---
+++ event_scripts.js
@@ -0,0 +1,22 @@
+
+function spotifyClick() {
+    fbq('track', 'ViewContent', {content_ids: ['Spotify'], content_type: 'product'});
+}
+function applemusicClick() {
+    fbq('track', 'ViewContent', {content_ids: ['Apple Music'], content_type: 'product'});
+}
+function bandcampClick() {
+    fbq('track', 'ViewContent', {content_ids: ['BandCamp'], content_type: 'product'});
+}
+function soundcloudClick() {
+    fbq('track', 'ViewContent', {content_ids: ['SoundCloud'], content_type: 'product'});
+}
+function instagramClick() {
+    fbq('track', 'ViewContent', {content_ids: ['Instagram'], content_type: 'product'});
+}
+function twitterClick() {
+    fbq('track', 'ViewContent', {content_ids: ['Twitter'], content_type: 'product'});
+}
+function youtubeClick() {
+    fbq('track', 'ViewContent', {content_ids: ['Youtube'], content_type: 'product'});
+}
